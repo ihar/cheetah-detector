cb4bf90429dac9fb092d9cb49e4390dde0ffaa23
Ihar Safonau <ihar.safonau@gmail.com>
2014-08-11 21:24:09 +0300

    More careful parameter search. Added calcultaion of final model.

--- parameter-sel/gradient-tree-boosting.py
+++ parameter-sel/gradient-tree-boosting.py
@@ -15,6 +15,7 @@ LBP_FEATURES = '../feature-gen/lbp-1-8.csv'
 
 COARSE_MODEL_FILE = './coarse-gtb.model'
 TUNED_MODEL_FILE = './tuned-gtb.model'
+FINAL_MODEL_FILE = './final-gtb.model'
 
 # Load distinct feature sets from two separate files,
 # make a combined feature for each image file.
@@ -30,7 +31,7 @@ def combine_features(bow_file, lbp_file):
 
     with open(lbp_file, 'rb') as csvfile:
         lbp_data = list(tuple(rec) for rec in csv.reader(csvfile, delimiter=','))
-    lbp_labels = np.array([int(seq[0]) for seq in lbp_data])
+    #lbp_labels = np.array([int(seq[0]) for seq in lbp_data])
     lbp_filenames = [seq[1] for seq in lbp_data]
 
     # Find a correspondence between rows of the two above files
@@ -64,16 +65,16 @@ if __name__ == '__main__':
     if (not os.path.exists(COARSE_MODEL_FILE)):
         # Find best parameter set for gradient boosted trees
         print strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " Find a semi-optimal parameter set for GBT..."
-        # param_grid = {'learning_rate': [0.1, 0.05, 0.01],
-        #               'max_depth': [4, 6],
-        #               'min_samples_leaf': [3, 9, 17],
-        #               'max_features': [1.0, 0.3, 0.1]}
-        param_grid = {'learning_rate': [0.1],
-                      'max_depth': [4],
-                      'min_samples_leaf': [9],
-                      'max_features': [1.0]}
-        sss = StratifiedShuffleSplit(class_labels, 3, test_size=0.25, random_state=530)
-        gscv = GridSearchCV(estimator=GradientBoostingRegressor(n_estimators=1000), param_grid=param_grid, scoring='roc_auc', n_jobs=3, cv=sss, verbose=2)
+        param_grid = {'learning_rate': [0.2, 0.1, 0.05],
+                      'max_depth': [4, 6],
+                      'min_samples_leaf': [3, 9, 17],
+                      'max_features': [1.0, 0.3, 0.1]}
+        # param_grid = {'learning_rate': [0.1],
+        #               'max_depth': [4],
+        #               'min_samples_leaf': [9],
+        #               'max_features': [1.0]}
+        sss = StratifiedShuffleSplit(class_labels, 5, test_size=0.25, random_state=530)
+        gscv = GridSearchCV(estimator=GradientBoostingRegressor(n_estimators=1000), param_grid=param_grid, scoring='roc_auc', n_jobs=4, cv=sss, verbose=2)
         gscv.fit(data_mat, class_labels)
 
         print "Best parameters set found on cv set:"
@@ -97,10 +98,10 @@ if __name__ == '__main__':
         coarse_gbt = read_gtb_model_from_file(COARSE_MODEL_FILE)
         print "Current parameters of the estimator:"
         print coarse_gbt.get_params()
-        param_grid = {'learning_rate': [0.1, 0.05, 0.025, 0.01],
+        param_grid = {'learning_rate': [0.5, 0.2, 0.1, 0.05, 0.025, 0.01],
                       'n_estimators': [2000, 3000]}
-        sss = StratifiedShuffleSplit(class_labels, 3, test_size=0.25, random_state=530)
-        gscv = GridSearchCV(estimator=coarse_gbt, param_grid=param_grid, scoring='roc_auc', n_jobs=3, cv=sss, verbose=2)
+        sss = StratifiedShuffleSplit(class_labels, 5, test_size=0.25, random_state=530)
+        gscv = GridSearchCV(estimator=coarse_gbt, param_grid=param_grid, scoring='roc_auc', n_jobs=4, cv=sss, verbose=2)
         gscv.fit(data_mat, class_labels)
         print "Best parameters set found on cv set:"
         print gscv.best_estimator_
@@ -114,6 +115,15 @@ if __name__ == '__main__':
             print("%0.3f (+/-%0.03f) for %r" % (mean_score, scores.std(), params))
 
         with open(TUNED_MODEL_FILE, 'wb') as fid:
-            cPickle.dump(gscv, fid)
+            cPickle.dump(gscv.best_estimator_, fid)
+
+    # And now train the best model on the whole data set
+    if (not os.path.exists((FINAL_MODEL_FILE))):
+        print strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " Train selected model on the whole data set."
+        tuned_gtb = read_gtb_model_from_file(TUNED_MODEL_FILE)
+        print tuned_gtb.get_params()
+        tuned_gtb.fit(data_mat, class_labels)
+        with open(FINAL_MODEL_FILE, 'wb') as fid:
+            cPickle.dump(tuned_gtb, fid)
 
     print strftime("%Y-%m-%d %H:%M:%S", gmtime()) + " Done."
